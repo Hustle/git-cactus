965a41ac80a3ade9a98516eee4b983f2e5e96114
Tyler Brock <tyler.brock@gmail.com>
2018-02-09 16:47:15 -0800

    Add ability to show diffs + approve cut or tag

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const util = require('util');
 const cp = require('child_process');
 const yargs = require('yargs');
 const logger = require('winston');
+const inquirer = require('inquirer');
 const semver = require('semver');
 const Git = require('nodegit');
 const GCH = require('git-credential-helper');
+
+// Enable pretty CLI logging
+logger.cli();
 
 // Custom fill promisification because callback is not final argument
 GCH.fill[util.promisify.custom] = function(url, options) {
@@ -25,8 +29,6 @@ GCH.fill[util.promisify.custom] = function(url, options) {
 // Promisify apis that have cb style
 const gchAvailable = util.promisify(GCH.available);
 const gchFill = util.promisify(GCH.fill);
-
-logger.cli();
 
 function wrap(fn) {
   return function(args) {
@@ -68,13 +70,49 @@ function makeGitOpts(credentials) {
   return options;
 }
 
-// HACK: run require of pacakge.json after using npm to manipulate it
-function getNextVersionInfo(packageJSONPath) {
-  const pkg = require(`${packageJSONPath}/package.json`);
-  const version = pkg.version;
+// HACK: this depends on the project being a NodeJS project
+// TODO: improve by allowing user to point you to file containing version info
+function getVersion(repoPath) {
+  if (!repoPath) {
+    repoPath = process.cwd();
+  }
+  const pkg = require(`${repoPath}/package.json`);
+  return pkg.version;
+}
+
+function generateNextVersion(currentVersion, level) {
+  const version = semver.inc(currentVersion, level)
   const minorVer = `v${semver.major(version)}.${semver.minor(version)}`;
   const releaseBranchName = `release-${minorVer}`;
   return { version, minorVer, releaseBranchName };
+}
+
+async function approveDiff(repo, currentVersion, nextVersion) {
+  const walk = Git.Revwalk.create(repo);
+  const endRange = nextVersion ? `v${nextVersion}` : 'master';
+  const range = `v${currentVersion}..${endRange}`;
+  walk.pushRange(range);
+  const commits = await walk.getCommitsUntil(() => true);
+
+  console.log(`--- START COMMIT LOG ${range} ---`);
+  commits.forEach((commit) => {
+    const sha = commit.sha();
+    const msg = commit.message();
+    const author = commit.author().name();
+    const comitter = commit.committer().name();
+    const date = commit.date();
+    console.log(`[${date}] (${author}) ${msg}`);
+  });
+  console.log(`--- END COMMIT LOG ${range} ---`);
+
+  return await inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'lgtm',
+      message: `Does the commit log look good?`,
+      default: false,
+    }
+  ]);
 }
 
 async function cutReleaseBranch(args) {
@@ -92,14 +130,24 @@ async function cutReleaseBranch(args) {
   const clonedRepo = await Git.Clone(url, tmpdir.name, options);
   const clonedRemote = await clonedRepo.getRemote('origin');
 
+  // Determine new version and branch names
+  const currentVersion = getVersion(tmpdir.name);
+  const versionInfo = generateNextVersion(currentVersion, args.level);
+
+  // Ask for approval on diff before cutting
+  logger.info('Cutting branch', versionInfo.releaseBranchName);
+  const approval = await approveDiff(repo, currentVersion);
+
   // Shell out to run npm version inside tempdir
   cp.execSync(`npm version ${args.level} -m "Release v%s"`, { cwd: tmpdir.name });
+  repo.refreshIndex();
 
-  // Determine new version and branch names
-  const versionInfo = getNextVersionInfo(tmpdir.name);
-  logger.info('Cutting branch', versionInfo.releaseBranchName);
+  if (!approval.lgtm) {
+    return 'Aborted push! Local changes not reverted, you must now do surgery!';
+  }
 
   // Push master, the release branch, and tag
+  logger.info(`Pushing branch ${versionInfo.releaseBranchName} & tag v${versionInfo.version}`);
   await clonedRemote.push([
     `refs/heads/master:refs/heads/master`,
     `refs/heads/master:refs/heads/${versionInfo.releaseBranchName}`,
@@ -115,11 +163,23 @@ async function tagVersion(args) {
   const remote = await repo.getRemote(args.upstream);
   const credentials = await getCreds(remote);
 
+  // Determine the next tag for this release branch (patch)
+  const currentVersion = getVersion();
+  const versionInfo = generateNextVersion(currentVersion, 'patch');
+
   // Shell out to run npm version
   cp.execSync('npm version patch -m "Release v%s"');
-  const versionInfo = getNextVersionInfo(process.cwd());
+
+  // Ask for approval on diff before pushing
+  logger.info('Tagging version', versionInfo.version);
+  const approval = await approveDiff(repo, currentVersion, versionInfo.version);
+
+  if (!approval.lgtm) {
+    return 'Aborted push! Local changes not reverted, you must now do surgery!';
+  }
 
   // Push updated release branch and new tag
+  logger.info('Pushing tagged version', versionInfo.version);
   await remote.push([
     `refs/heads/${versionInfo.releaseBranchName}:refs/heads/${versionInfo.releaseBranchName}`,
     `refs/tags/v${versionInfo.version}:refs/tags/v${versionInfo.version}`,
